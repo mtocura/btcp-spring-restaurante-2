574224eb7ed75a82e5589169cc50be67c7441045
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-02 14:26:31 -0300

    adicionando propriedades

--- src/main/java/br/com/aula3/tm/restaurante2/dto/PedidoDTO.java
+++ src/main/java/br/com/aula3/tm/restaurante2/dto/PedidoDTO.java
@@ -4,17 +4,29 @@ import java.util.List;
 
 public class PedidoDTO {
     private List<PratoDTO> pratos;
+    private String status;
+    private String data;
     private Double totalPedido;
 
     public PedidoDTO() {}
 
-    public PedidoDTO(List<PratoDTO> pratos, Double totalPedido) {
+    public PedidoDTO(List<PratoDTO> pratos, String status, String data, Double totalPedido) {
         this.pratos = pratos;
+        this.status = status;
+        this.data = data;
         this.totalPedido = totalPedido;
     }
 
     public List<PratoDTO> getPratos() {
         return pratos;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getData() {
+        return data;
     }
 
     public Double getTotalPedido() {
