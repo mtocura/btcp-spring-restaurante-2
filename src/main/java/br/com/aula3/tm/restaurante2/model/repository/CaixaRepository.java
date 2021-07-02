85502063fe80397874b72af437c80e70304a9d0b
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-02 16:03:16 -0300

    Criação do repository para o Caixa

---
+++ src/main/java/br/com/aula3/tm/restaurante2/model/repository/CaixaRepository.java
@@ -0,0 +1,66 @@
+package br.com.aula3.tm.restaurante2.model.repository;
+
+import br.com.aula3.tm.restaurante2.model.entity.Caixa;
+import br.com.aula3.tm.restaurante2.model.entity.Mesa;
+import br.com.aula3.tm.restaurante2.model.entity.Pedido;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class CaixaRepository {
+    private static final File FILE = new File("restaurante.json");
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    public List<Caixa> getAll(){
+        List<Caixa> anuncios = new ArrayList<>();
+        try {
+            FileInputStream is = new FileInputStream(FILE);
+            TypeReference<List<Caixa>> typeReference = new TypeReference<List<Caixa>>() {};
+            anuncios = mapper.readValue(is, typeReference);
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return anuncios;
+    }
+
+    public List<Caixa> getPedidosData(String data) {
+        List<Caixa> registros = getAll();
+        List<Caixa> res = new ArrayList<>();
+
+        for (Caixa registro : registros) {
+            if(registro.getData().equals(data)) {
+                res.add(registro);
+            }
+        }
+
+        return res;
+    }
+
+    public void addRegistro(Caixa caixa) {
+        try {
+            List<Caixa> registros = getAll();
+            registros.add(caixa);
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(FILE)));
+            mapper.writeValue(out, registros);
+            out.close();
+        } catch (JsonGenerationException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
