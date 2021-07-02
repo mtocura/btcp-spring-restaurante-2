e3362a910284982628dc913888bdd03af46144a0
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-02 12:40:40 -0300

    adicionando atributos faltantes

--- src/main/java/br/com/aula3/tm/restaurante2/model/entity/Pedido.java
+++ src/main/java/br/com/aula3/tm/restaurante2/model/entity/Pedido.java
@@ -1,18 +1,23 @@
 package br.com.aula3.tm.restaurante2.model.entity;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class Pedido {
     private Long id;
     private Mesa mesa;
+    private String status;
+    private String data;
     private List<Prato> pratos;
     private Double total;
 
     public Pedido() {}
 
-    public Pedido(Long id, Mesa mesa, List<Prato> pratos, Double total) {
+    public Pedido(Long id, Mesa mesa, String status, String data, List<Prato> pratos, Double total) {
         this.id = id;
         this.mesa = mesa;
+        this.status = status;
+        this.data = data;
         this.pratos = pratos;
         this.total = total;
     }
@@ -23,6 +28,14 @@ public class Pedido {
 
     public Mesa getMesa() {
         return mesa;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getData() {
+        return data;
     }
 
     public List<Prato> getPratos() {
