51b8c2134db892ff081302b68fa121d1a078e923
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-02 12:21:10 -0300

    inicialização do projeto spring boot

---
+++ src/main/java/br/com/aula3/tm/restaurante2/Restaurante2Application.java
@@ -0,0 +1,13 @@
+package br.com.aula3.tm.restaurante2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restaurante2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Restaurante2Application.class, args);
+    }
+
+}
