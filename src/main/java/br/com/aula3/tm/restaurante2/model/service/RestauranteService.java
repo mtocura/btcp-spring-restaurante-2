d0976bc2eca8b43156dc4646c3e7a178bb66af56
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-02 15:17:06 -0300

    criação do service para Restaurante

---
+++ src/main/java/br/com/aula3/tm/restaurante2/model/service/RestauranteService.java
@@ -0,0 +1,53 @@
+package br.com.aula3.tm.restaurante2.model.service;
+
+import br.com.aula3.tm.restaurante2.dto.MesaDTO;
+import br.com.aula3.tm.restaurante2.dto.PedidoDTO;
+import br.com.aula3.tm.restaurante2.form.MesaForm;
+import br.com.aula3.tm.restaurante2.model.entity.Mesa;
+import br.com.aula3.tm.restaurante2.model.repository.RestauranteRepository;
+import br.com.aula3.tm.restaurante2.utils.converter.MesaConverter;
+import br.com.aula3.tm.restaurante2.utils.converter.PedidoConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RestauranteService {
+    private RestauranteRepository restauranteRepository;
+
+    public RestauranteService() {}
+
+    @Autowired
+    public RestauranteService(RestauranteRepository restauranteRepository) {
+        this.restauranteRepository = restauranteRepository;
+    }
+
+    public List<MesaDTO> findAll() {
+        return MesaConverter.mesaEntityToDTO(restauranteRepository.getAll());
+    }
+
+    public List<PedidoDTO> getPedidosMesa(long id) {
+        return PedidoConverter.pedidoEntityToDTO(restauranteRepository.getPedidosMesa(id));
+    }
+
+    public List<PedidoDTO> getPedidosData(String data) {
+        // TODO validação de data
+        return PedidoConverter.pedidoEntityToDTO(restauranteRepository.getPedidosData(data));
+    }
+
+    public Mesa addMesa(MesaForm mesaForm) {
+        Mesa mesa = MesaConverter.mesaFormToEntity(mesaForm);
+        restauranteRepository.addMesa(mesa);
+        return mesa;
+    }
+
+    public void updatedMesa(long id, MesaForm mesaForm) {
+        Mesa mesa = MesaConverter.mesaFormToEntity(mesaForm);
+        restauranteRepository.updateMesa(mesa, id);
+    }
+
+    public MesaDTO deleteMesa(long id) {
+       return MesaConverter.mesaEntityToDTO(restauranteRepository.deleteMesa(id));
+    }
+}
